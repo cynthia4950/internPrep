c4ad343bdc5292d4aacae757eb97c66185e4d131
jasperleixu <jasperleixu@tencent.com>
2021-04-09 19:07:52 +0800

    一些意见 V1.0

--- multiwayMS/consumer/consumer.go
+++ multiwayMS/consumer/consumer.go
@@ -1,34 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
-	"strconv"
-	"encoding/json"
+
 	"github.com/adjust/rmq/v3"
 )
 
 const (
-	prefetchLimit = 1
-	pollDuration  = 100 * time.Millisecond
-	numConsumers  = 1
-	inputSize = 100000
-	numArrays = 10
-	maxint32 = 2147483647
+	prefetchLimit   = 1
+	pollDuration    = 100 * time.Millisecond
+	numConsumers    = 1
+	inputSize       = 100000
+	numArrays       = 10
+	maxint32        = 2147483647
 	consumeDuration = time.Millisecond
-
 )
-
 
 type ReceivingManager interface {
 	OpenConnAndProcess()
 }
 
 type RealReceive struct {
-	port 	string
+	port string
 }
 
 /*
@@ -37,55 +36,59 @@ type FakeReceive struct {
 }
 */
 
-
 type Batch struct {
-	Id     int	`json:"id"`
-	Nums	[]int	`json:"nums"`
+	Id   int   `json:"id"`
+	Nums []int `json:"nums"`
 }
 
 type Consumer struct {
-	id  int
+	id int
 }
 
 type fakeConsumer struct {
-	id  int
+	id   int
 	fake bool
 }
 
+//不要使用全局变量，多协程+多线程同时操作时，会出现coredump或者内存混乱
+//变量的作用域越小，代码越清晰易懂
 var allNums [][]int
 var indexPtrs []int
 var complete []bool
 var allComplete bool
 
-
 func NewConsumer(tag int) *Consumer {
 	return &Consumer{
-		id: 	tag,
+		id: tag,
 	}
 }
 
 func NewFakeConsumer(tag int) *fakeConsumer {
 	return &fakeConsumer{
-		id: 	tag,
+		id: tag,
 	}
 }
 
-func createOutputFile(store **os.File, fileName string) bool{
+//store直接作为返回值即可，考虑func createOutputFile(fileName string) （store *os.File err error)
+func createOutputFile(store **os.File, fileName string) bool {
 	temp, err := os.Create(fileName)
 	if err != nil {
-		panic(err)
+		panic(err) //正式程序，不要有任何panic的行为，返回error即可
 	}
 	*store = temp
 	return true
 }
 
-func findMinNum(complete *[]bool, allNums *[][]int, indexPtrs *[]int, row_size int) (int,int){
+//complete *[]bool用法不对，数组本身是传址，不需要指针。后面的参数同理。
+func findMinNum(complete *[]bool, allNums *[][]int, indexPtrs *[]int, row_size int) (int, int) {
 	tempMin := maxint32
 	minArr := 0
-	for i := 0; i < row_size; i++{
+
+	//go的数组不是C的数组，实际上用len(complete)就能获取
+	for i := 0; i < row_size; i++ {
 		if (*complete)[i] {
 			// fmt.Println("skip the array at index " + strconv.Itoa(minArr))
-			continue;
+			continue
 		}
 		//workingArr is the array we are currently checking it's next number
 		workingArr := (*allNums)[i]
@@ -98,7 +101,7 @@ func findMinNum(complete *[]bool, allNums *[][]int, indexPtrs *[]int, row_size i
 		//the next number
 		workingNum := workingArr[workingIndex]
 
-		if(workingNum <= tempMin){
+		if workingNum <= tempMin {
 			tempMin = workingNum
 			minArr = i
 		}
@@ -107,12 +110,13 @@ func findMinNum(complete *[]bool, allNums *[][]int, indexPtrs *[]int, row_size i
 	return tempMin, minArr
 }
 
-func checkCompleArr(minArr int, colSize int, indexPtrs *[]int, complete *[]bool, allComplete *bool) bool{
+//allComplete作为入参没有用，直接作为返回值即可
+func checkCompleArr(minArr int, colSize int, indexPtrs *[]int, complete *[]bool, allComplete *bool) bool {
 	//minArr is the index of the array whose number being selected is the minunum
 	//complete is an aray of boolean marking which array has been finished
 	//allComplete mark if all arrays are finished
 
-	(*indexPtrs)[minArr]++;
+	(*indexPtrs)[minArr]++
 	if (*indexPtrs)[minArr] >= colSize {
 		(*complete)[minArr] = true
 		*allComplete = true
@@ -126,7 +130,7 @@ func checkCompleArr(minArr int, colSize int, indexPtrs *[]int, complete *[]bool,
 	return *allComplete
 }
 
-func writeToFile(fileHandle *os.File, content int) bool{
+func writeToFile(fileHandle *os.File, content int) bool {
 	_, err := fileHandle.WriteString(fmt.Sprintf("%d\n", content))
 	if err != nil {
 		fmt.Println(err)
@@ -134,19 +138,18 @@ func writeToFile(fileHandle *os.File, content int) bool{
 	return true
 }
 
-
-func merge(complete []bool, allNums [][]int, indexPtrs []int, numArrays int, inputSize int) []int{
+//numArrays 全局常量和参数重名，易产生bug
+func merge(complete []bool, allNums [][]int, indexPtrs []int, numArrays int, inputSize int) []int {
 	fmt.Println("in merge")
-	
+
 	var res []int
 	var fileHandle *os.File
 	// var fileHandle *os.File
 	createSuccess := createOutputFile(&fileHandle, "data/output.txt")
-	if createSuccess{
+	if createSuccess {
 		defer fileHandle.Close()
 	}
-		
-	
+
 	// minArr: the index of the array whose number pointed by the pointer is the minimum at this turn
 	// test_count := 0
 	allComplete = false
@@ -157,30 +160,30 @@ func merge(complete []bool, allNums [][]int, indexPtrs []int, numArrays int, inp
 		writeToFile(fileHandle, tempMin)
 	}
 
-
 	fmt.Println("end merge: Ten payloads have been consumed")
 	return res
-	
+
 }
 
 func unmarshallPayload(delivery rmq.Delivery) Batch {
 	var task Batch
-    if err := json.Unmarshal([]byte(delivery.Payload()), &task); err != nil {
-        // handle json error
-        if err := delivery.Reject(); err != nil {
-            // handle reject error
+	if err := json.Unmarshal([]byte(delivery.Payload()), &task); err != nil {
+		// handle json error
+		if err := delivery.Reject(); err != nil {
+			// handle reject error
 			fmt.Println(err)
-        }
-    }
+		}
+	}
 	return task
 }
 
-func append_payload(task Batch, original *[][]int) [][]int{
-	*original = append(*original,task.Nums)
+func append_payload(task Batch, original *[][]int) [][]int {
+	//多线程+协程场景，会有问题
+	*original = append(*original, task.Nums)
 	return *original
 }
 
-func mergeTenBatches(allNums[][]int, numArrays int) []int{
+func mergeTenBatches(allNums [][]int, numArrays int) []int {
 	var res []int
 	if len(allNums) == numArrays {
 		res = merge(complete, allNums, indexPtrs, numArrays, inputSize)
@@ -192,38 +195,39 @@ func (consumer *Consumer) Consume(delivery rmq.Delivery) {
 	// fmt.Println("In Comsume(), id: " + strconv.Itoa(consumer.id))
 	task := unmarshallPayload(delivery)
 
-	// fmt.Println("task.id: " + strconv.Itoa(task.Id)) 
+	// fmt.Println("task.id: " + strconv.Itoa(task.Id))
 	// fmt.Println("task.Nums size: " + strconv.Itoa(len(task.Nums)))
 	append_payload(task, &allNums)
 
-    // perform task
-    if err := delivery.Ack(); err != nil {
-        // handle ack error
+	// perform task
+	if err := delivery.Ack(); err != nil {
+		// handle ack error
 		fmt.Println(err)
-    }
+	}
 
-	mergeTenBatches(allNums,numArrays)
+	mergeTenBatches(allNums, numArrays)
 }
 
 func (consumer *fakeConsumer) Consume(delivery rmq.Delivery, fake_allNums *[][]int, fake_numArrays *int) {
 	// fmt.Println("In Comsume(), id: " + strconv.Itoa(consumer.id))
 	task := unmarshallPayload(delivery)
 
-	// fmt.Println("task.id: " + strconv.Itoa(task.Id)) 
+	// fmt.Println("task.id: " + strconv.Itoa(task.Id))
 	// fmt.Println("task.Nums size: " + strconv.Itoa(len(task.Nums)))
 	append_payload(task, fake_allNums)
 
-	mergeTenBatches(*fake_allNums,*fake_numArrays)
+	mergeTenBatches(*fake_allNums, *fake_numArrays)
 }
 
-func (consumer_real *RealReceive) OpenConnAndProcess(){
+//考虑函数原型 func (consumer_real *RealReceive) OpenConnAndProcess() (err error)
+func (consumer_real *RealReceive) OpenConnAndProcess() {
 	errChan := make(chan error, 10)
 	// go logErrors(errChan)
 
 	connection, err := rmq.OpenConnection("consumer", "tcp", consumer_real.port, 1, errChan)
 	// defer connection.Close()
 	if err != nil {
-		panic(err)
+		panic(err) //不要panic
 	}
 
 	queue, err := connection.OpenQueue("num_queue")
@@ -240,10 +244,9 @@ func (consumer_real *RealReceive) OpenConnAndProcess(){
 	if _, err := queue.AddConsumer("consumer", NewConsumer(0)); err != nil {
 		panic(err)
 	}
-	
+
 	fmt.Println("in comsumer main, after add consumer")
-	
-	
+
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT)
 	defer signal.Stop(signals)
@@ -255,10 +258,8 @@ func (consumer_real *RealReceive) OpenConnAndProcess(){
 	}()
 	// queue.StopConsuming()
 	<-connection.StopAllConsuming() // wait for all Consume() calls to finish
-	
+
 }
-
-
 
 func main() {
 	// fmt.Println("in comsumer main")
